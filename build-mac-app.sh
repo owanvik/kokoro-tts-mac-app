b099ec1fbd7fa6ada3b2859f50c5c61ad9c6a631
Olai Wanvik <olai.wanvik@reitaneiendom.no>
2026-03-11 21:56:01 +0100

    Add requirements.txt, install deps in build script

--- build-mac-app.sh
+++ build-mac-app.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 cd "$(dirname "$0")"
 
 source .venv/bin/activate
-python -m pip install -q pyinstaller
+python -m pip install -q -r requirements.txt
 
 VERSION=$(cat VERSION | tr -d '\n')
 
